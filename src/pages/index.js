fa03b15f6cbd4ae02a89bcc3dfc73a9704990902
ap3334 <chwil2989@naver.com>
2022-06-29 14:06:47 +0900

    Head 컴포넌트 도입

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
-import { Link } from "gatsby"
 import React from "react"
+import Head from "../components/head"
 
 // markup
 const IndexPage = () => {
@@ -7,12 +7,7 @@ const IndexPage = () => {
     <main>
       <title>Home Page</title>
 
-      <div style={{display: "flex", gap:10}}>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/test">Test</Link>
-        <Link to="/test-space">Test Space</Link>
-      </div>
+      <Head />
 
       <h1>Home Page</h1>
     </main>
--- src/pages/test.js
+++ src/pages/test.js
@@ -1,17 +1,12 @@
-import { Link } from "gatsby";
 import React from "react";
+import Head from "../components/head";
 
 function TestPage() {
     return (
         <main>
             <title>테스트</title>
 
-            <div style={{display: "flex", gap:10}}>
-                <Link to="/">Home</Link>
-                <Link to="/about">About</Link>
-                <Link to="/test">Test</Link>
-                <Link to="/test-space">Test Space</Link>
-            </div>
+            <Head />
 
             <h1>테스트 페이지입니다.</h1>
         </main>
